e27cdda601a16b325453251efb11c10a79a1e912
Rustam-Asdanov <Rustam-Asdanov@users.noreply.github.com>
2021-07-09 23:35:06 +0400

    create project with using hibernate. One to one uni relationship

--- hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class TestJdbc {
 	public static void main(String[] args) {
 		
-		String jdbcUrl = "jdbc:mysql://localhost:3310/hb_student_tracker?useSSL=false";
+		String jdbcUrl = "jdbc:mysql://localhost:3310/hb-01-one-to-one-uni?useSSL=false";
 		String user = "root";
 		String pass = "11111";
 		
